d0f3f7d514e164c935c71f9edeff80b1a5459e97
skydereign <lsdawson@ucsc.edu>
2013-03-05 01:16:17 +0000

    Rolling back to c51 due to accidental commit.

--- Player.js
+++ Player.js
@@ -150,10 +150,6 @@ Player.prototype.keyd_right = function () {
     case 3:
 	this.state = 2; // run right
 	break;
-
-    case 5:
-	this.state = 4;
-	break;
     }
     this.x_velocity = 3;;
     this.changeAnimation(this.state);
@@ -165,10 +161,6 @@ Player.prototype.keyd_left = function () {
     case 1:
     case 2:
 	this.state = 3; // run left
-	break;
-	
-    case 4:
-	this.state = 5; // jump left
 	break;
     }
     this.x_velocity = -3;
