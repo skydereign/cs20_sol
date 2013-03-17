3c59b82fc91c1c56812304a94cb7bd686b32980b
skydereign <lsdawson@ucsc.edu>
2013-03-17 02:40:07 +0000

    Bug fix: green light deceleration works again.

--- Player.js
+++ Player.js
@@ -350,7 +350,7 @@ Player.prototype.keyd_right = function () {
 						this.state = 4; // jump right
 						break;
 				}
-				this.x_velocity = 3 +3*this.gmult*this.light.g;
+				this.x_velocity = 3 +3*this.gmult*this.light.gspeed;
 				this.changeAnimation(this.state);
 		}
 }
@@ -368,7 +368,7 @@ Player.prototype.keyd_left = function () {
 						this.state = 5; // jump left
 						break;
 				}
-				this.x_velocity = -3-3*this.gmult*this.light.g;
+				this.x_velocity = -3-3*this.gmult*this.light.gspeed;
 				this.changeAnimation(this.state);
 		} else {
 				this.x_velocity-=0.25;
